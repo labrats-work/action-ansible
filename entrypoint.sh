2c2f4dc5fccee1e309b8a596214aed9ce55dde49
tomp736 <12602834+tomp736@users.noreply.github.com>
2022-11-23 13:29:17 +0100

    revert entrypoint chanes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,11 @@
 #!/bin/sh -l
 
-which ansible
+# get version
+ansible_version=$(ansible --version | head -n 1)
 
-# Print version
-ansible --version
-
-ansible_version="test"
-
+# output version
 echo "ansible_version=$ansible_version" >> $GITHUB_OUTPUT
-# Print requirements file
+# output ansible_requirements
 echo "ansible_requirements=$1" >> $GITHUB_OUTPUT
-# Print inventory file
+# output ansible_inventory
 echo "ansible_inventory=$2" >> $GITHUB_OUTPUT
