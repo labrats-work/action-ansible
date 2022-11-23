4bcdfeaaa4c9e9643f456aba3ac93153b37148df
tomp736 <12602834+tomp736@users.noreply.github.com>
2022-11-23 13:43:07 +0100

    tests

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,23 @@
 #!/bin/sh -l
 
 # get version
-ansible_version=$(ansible --version | head -n 1)
+nsbl_version=$(ansible --version | head -n 1)
+nsbl_requirements=$1
+nsbl_inventory=$2
+nsbl_playbook=$3
+nsbl_workingdirectory=$4
 
-# output version
-echo "ansible_version=$ansible_version" >> $GITHUB_OUTPUT
-# output ansible_requirements
-echo "ansible_requirements=$1" >> $GITHUB_OUTPUT
-# output ansible_inventory
-echo "ansible_inventory=$2" >> $GITHUB_OUTPUT
+# output nsbl_version
+echo "nsbl_version=$nsbl_version" >> $GITHUB_OUTPUT
+# output nsbl_requirements
+echo "nsbl_requirements=$nsbl_requirements" >> $GITHUB_OUTPUT
+# output nsbl_inventory
+echo "nsbl_inventory=$nsbl_inventory" >> $GITHUB_OUTPUT
+# output nsbl_playbook
+echo "nsbl_playbook=$nsbl_playbook" >> $GITHUB_OUTPUT
+
+cd $nsbl_workingdirectory
+
+ansible-galaxy install -r $ansible_requirements
+
+ansible-playbook -i $ansible_inventory $ansible_playbook
