5f0162f26c16c6e05638ea3ea9ce6e92c9584c31
tomp736 <12602834+tomp736@users.noreply.github.com>
2022-11-23 17:32:43 +0100

    drop ssh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,6 @@ nsbl_requirements=$1
 nsbl_inventory=$2
 nsbl_playbook=$3
 nsbl_workingdirectory=$4
-nsbl_sshkey=$5
 
 # output nsbl_version
 echo "nsbl_version=$nsbl_version" >> $GITHUB_OUTPUT
@@ -18,14 +17,6 @@ echo "nsbl_inventory=$nsbl_inventory" >> $GITHUB_OUTPUT
 echo "nsbl_playbook=$nsbl_playbook" >> $GITHUB_OUTPUT
 
 cd $nsbl_workingdirectory
-
-if [ -z "$nsbl_sshkey" ]
-then
-    echo "Adding ssh-key to agent"
-    eval `ssh-agent`
-    echo "$nsbl_sshkey" | ssh-add - > /dev/null
-    ssh-add -l
-fi
 
 ansible-galaxy install -r $nsbl_requirements
 
